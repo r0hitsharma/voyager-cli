b85281cccf3c719c44015dcf8e79f2c287e9a722
r0hitsharma <rs@rohit.sh>
2023-03-11 21:55:39 +0100

    Check file format.

--- index.ts
+++ index.ts
@@ -11,6 +11,14 @@ program.parse();
 const args = program.args;
 const [file] = args;
 
+const supportedFileFromats = ['csv'] as const;
+const isSupported = supportedFileFromats.some(format => file.endsWith(`.${format}`));
+
+if(!isSupported){
+    console.error(`Please use one of the following formats: ${supportedFileFromats.join(', ')}`);
+    process.exit(1);
+}
+
 try {
     (async function () {
         await readFile(file);

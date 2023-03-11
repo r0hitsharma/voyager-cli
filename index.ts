eaed00189669cc8458f62d929102b1b1fb1762b8
r0hitsharma <rs@rohit.sh>
2023-03-11 23:46:27 +0100

    Add express and start client.

--- index.ts
+++ index.ts
@@ -3,14 +3,17 @@
 import { readFile } from 'fs/promises';
 import { Command } from 'commander';
 import { parse } from 'csv-parse/sync';
+import express from 'express';
+
+const PORT = 8080;
 
 const program = new Command();
 program.argument('<file>', 'file to preview');
+program.option('-p, --port', 'port for binding client', PORT.toString());
 
 program.parse();
-
-const args = program.args;
-const [file] = args;
+const [ file ] = program.args;
+const { port } = program.opts();
 
 const supportedFileFromats = ['csv'] as const;
 const isSupported = supportedFileFromats.some(format => file.endsWith(`.${format}`));
@@ -23,8 +26,22 @@ if(!isSupported){
 try {
     (async function () {
         const contents = await readFile(file);
-        const records = parse(contents);
+        const records = parse(contents, {
+            columns: true
+        });
         console.log(`File contains ${records.length} records`);
+
+        const app = express();
+
+        app.use(express.static('public'));
+
+        app.get('/data', (req, res) => {
+            res.json(records);
+        });
+
+        app.listen(port, () => {
+            console.log(`Client listening at http://localhost:${port}`);
+        });
     })();
 } catch (err){
     console.error(err?.message);

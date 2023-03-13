a8bf8002ecca1e3559fda12b2d1a174eba440636
r0hitsharma <rs@rohit.sh>
2023-03-13 16:58:07 +0100

    Untrack built bundle and add build script and source.

---
+++ public/main.js
@@ -0,0 +1,10 @@
+var libvoyager = require('datavoyager');
+
+var container = document.getElementById("root");
+var config = {
+  showDataSourceSelector: false,
+  hideHeader: true,
+  hideFooter: true
+}
+
+global.voyagerInstance = libvoyager.CreateVoyager(container, config, undefined);
